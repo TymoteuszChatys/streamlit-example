0dc00e6a26f509927b36b1f62aeb0981f4f31ceb
tymoteuszchatys <tymoteuszchatys@gmail.com>
2021-12-16 14:32:48 +0000

    removed libraries not in use

--- model.py
+++ model.py
@@ -1,14 +1,10 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
-import gzip
 from sklearn.naive_bayes import GaussianNB
 from sklearn.linear_model import LogisticRegression
 from sklearn.linear_model import RidgeClassifier
 from sklearn.linear_model import PassiveAggressiveClassifier,SGDClassifier, Perceptron
-from sklearn.metrics import confusion_matrix
 from sklearn.metrics import f1_score
-from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 import re
 import nltk
@@ -16,7 +12,6 @@ nltk.download('stopwords')
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 from sklearn.feature_extraction.text import CountVectorizer
-import matplotlib.pyplot as plt
 
 
 
