e1eb1e313c252e47c3a103c28fb974575ad3ab4f
tymoteuszchatys <tymoteuszchatys@gmail.com>
2021-12-16 14:14:09 +0000

    test change

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ In the meantime, below is an example of what you can do with just a few lines of
 question = st.selectbox("What question do you want?", ["How tall is this?", "how wide is this?"], index=0)
 answer = st.text_area("What's the answer" , value="")
 
-if st.button("Let's go", key=None, help=None, on_click=None, args=None, kwargs=None):
+if st.button("Is this answer helpful?", key=None, help=None, on_click=None, args=None, kwargs=None):
   st.text(f'Your question was {question}, your answer was {answer}')
 
 """
