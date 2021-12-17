9fd739c72ae0f7ecfdefea55dd9e23ecdb87dc13
tymoteuszchatys <tymoteuszchatys@gmail.com>
2021-12-17 10:07:24 +0000

    change text

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,8 @@ import model
 
 final_models,model_cvs,names = model.start()
 
-question = st.selectbox("What question do you want?", ["How tall is this?", "how wide is this?"], index=0)
-answer = st.text_area("What's the answer" , value="")
+question = st.selectbox("Question", ["Does this come with batteries?", "How wide is this?"], index=0)
+answer = st.text_area("Answer" , value="")
 
 if st.button("Is this answer helpful?", key=None, help=None, on_click=None, args=None, kwargs=None):
     df,prediction = model.predict(question,answer,final_models,model_cvs,names)
