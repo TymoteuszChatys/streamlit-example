a4e171526b04da2a999b3c40d52b710a950fc152
tymoteuszchatys <tymoteuszchatys@gmail.com>
2021-12-17 10:01:38 +0000

    remove unnecessary code

--- model.py
+++ model.py
@@ -45,9 +45,6 @@ def predict(question,answer,final_models,model_cvs,names):
 
 
 def start():
-    #dataset = load_data()
-    #corpus = populate_corpus(dataset)
-    #models,features = prepare_models()
     final_models,model_cvs,names = load_models()
     return final_models,model_cvs,names
 
@@ -60,66 +57,3 @@ def load_models():
     f.close()
 
     return models,cvs,names
-
-
-def load_data():
-    dataset = pd.read_csv("DATA_EDITED.csv")
-    dataset = dataset.dropna()
-    dataset = dataset.reset_index(drop=True)
-
-    return dataset
-
-def populate_corpus(dataset):
-    corpus = []
-    for i in range(len(dataset['answer'])):
-        review = re.sub("[^a-zA-z]", ' ', dataset["answer"][i])
-        review = review.lower()
-        review = review.split()
-        ps = PorterStemmer()
-        review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))]
-        review = " ".join(review)
-        corpus.append(review)
-
-    return corpus
-
-def prepare_models():
-    models = []
-    features = []
-
-    #models.append(GaussianNB())
-    #features.append(350)
-
-    models.append(LogisticRegression())
-    features.append(325)
-
-    models.append(RidgeClassifier())
-    features.append(350)
-
-    models.append(RidgeClassifier())
-    features.append(175)
-
-    models.append(SGDClassifier(loss="epsilon_insensitive"))
-    features.append(300)
-
-    models.append(SGDClassifier(loss="epsilon_insensitive"))
-    features.append(150)
-
-    return models,features
-
-def train_models(models,features,dataset,corpus):
-    final_models = []
-    model_cvs = []
-
-    for model,n_features in zip(models,features):
-        cv = CountVectorizer(max_features = n_features)
-        X_pre = cv.fit_transform(corpus)
-        X = X_pre.toarray()
-        y = dataset.tag
-
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0, stratify=y)
-        model.fit(X_train, y_train)
-        
-        final_models.append(model)
-        model_cvs.append(cv)
-
-        return final_models,model_cvs
