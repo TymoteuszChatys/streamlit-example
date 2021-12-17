d8948c5c05e260cb235de568c3bcc5d5f818687b
tymoteuszchatys <tymoteuszchatys@gmail.com>
2021-12-17 09:59:59 +0000

    change ordering

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,11 +10,11 @@ question = st.selectbox("What question do you want?", ["How tall is this?", "how
 answer = st.text_area("What's the answer" , value="")
 
 if st.button("Is this answer helpful?", key=None, help=None, on_click=None, args=None, kwargs=None):
-  df,prediction = model.predict(question,answer,final_models,model_cvs,names)
-  
-  st.text(f'Your answer was {prediction}')
-  st.text(f'Veto power.')
-  st.text(f"It only takes one of the models below to determine if your answer is unhelpful.")
-  st.text(f"1 - Unhelpful")
-  st.text(f"0 - Helpful!")
-  st.table(df)
+    df,prediction = model.predict(question,answer,final_models,model_cvs,names)
+    
+    st.text(f'Your answer was {prediction}')
+    st.table(df)
+    st.text(f'Veto power.')
+    st.text(f"It only takes one of the models below to determine if your answer is unhelpful.")
+    st.text(f"1 - Unhelpful")
+    st.text(f"0 - Helpful!")
