8d0059d5042935a8ad61f975336e061458dc3730
tymoteuszchatys <tymoteuszchatys@gmail.com>
2021-12-17 10:10:40 +0000

    Alter view

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,10 +12,15 @@ answer = st.text_area("Answer" , value="")
 if st.button("Is this answer helpful?", key=None, help=None, on_click=None, args=None, kwargs=None):
     df,prediction = model.predict(question,answer,final_models,model_cvs,names)
     
+    st.markdown("""---""")
     st.header(f'Your answer was {prediction}')
     st.markdown("""---""")
+
+
+    st.subheader("Summary")
     st.table(df)
     st.text(f'Veto power.')
     st.text(f"We just need one model to flag your answer as unhelpful.")
     st.text(f"1 - Unhelpful")
     st.text(f"0 - Helpful!")
+    st.markdown("""---""")
